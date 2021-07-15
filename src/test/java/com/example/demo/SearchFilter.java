6d6f37f89bfa89d8385e962a522ac7c13297e85c
Owen Jaques <owenjaques2000@gmail.com>
2021-07-14 23:11:22 -0700

    finished search / filter tests

--- src/test/java/com/example/demo/SearchFilter.java
+++ src/test/java/com/example/demo/SearchFilter.java
@@ -23,12 +23,9 @@ public class SearchFilter {
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "D:\\My Documents\\Uvic OneDrive\\OneDrive - University of Victoria\\School\\SENG 275\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\owenj\\NotOneDrive\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://automationpractice.com/index.php");
-
-        search = driver.findElement(By.id("search_query_top"));
     }
 
     @AfterMethod
@@ -39,6 +36,8 @@ public class SearchFilter {
     //TC_SF_001
     @Test
     public void validSearch(){
+        driver.get("http://automationpractice.com/index.php");
+        search = driver.findElement(By.id("search_query_top"));
         search.sendKeys("blouse");
         search.sendKeys(Keys.RETURN);
         assertTrue(driver.findElements(By.cssSelector(".ajax_block_product")).size() > 0);
@@ -59,6 +58,8 @@ public class SearchFilter {
     //TC_SF_002
     @Test
     public void invalidSearch(){
+        driver.get("http://automationpractice.com/index.php");
+        search = driver.findElement(By.id("search_query_top"));
         search.sendKeys("asdf");
         search.sendKeys(Keys.RETURN);
         assertTrue(driver.findElements(By.cssSelector(".ajax_block_product")).size() == 0);
@@ -96,5 +97,107 @@ public class SearchFilter {
         }
     }
 
+    //TC_SF_005
+    @Test
+    public void filterCategory() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
 
+        // filter by a category
+        driver.findElement(By.id("layered_category_4")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+            .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_006
+    @Test
+    public void filterSize() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.name("layered_id_attribute_group_1")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+            .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_007
+    @Test
+    public void filterCompositions() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.name("layered_id_feature_5")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_008
+    @Test
+    public void filterStyles() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.name("layered_id_feature_5")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_009
+    @Test
+    public void filterProperties() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.name("layered_id_feature_5")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[3]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_010
+    @Test
+    public void filterAvailability() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.xpath("//*[@id=\"ul_layered_id_feature_6\"]/li[2]")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_011
+    @Test
+    public void filterManufacturer() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.id("layered_manufacturer_1")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
+
+    //TC_SF_012
+    @Test
+    public void filterCondition() {
+        driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");
+
+        // filter by a category
+        driver.findElement(By.id("llayered_condition_new")).click();
+
+        // still seeming to hang forever so the test will most likely fail here
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/ul/li[2]/div/div[1]/div/a[1]/img")));
+    }
 }
